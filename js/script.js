3141bfdb6a1c9e21ce1bd41b75507e4805d65fdc
stachkrol <st.krolikiewicz@gmail.com>
2022-10-15 14:03:46 +0200

    Working on layout; updating CSS

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ function addTask() {
     <div class="row">
         <div class="col-12">
             <label>
-                <input type="checkbox" name="task-chekbox" id="task-checkbox">
-                <input type="text" name="task-content" id="task-content">
+                <input type="checkbox" name="task-chekbox" id="task-checkbox" class="task-input input-item">
+                <input type="text" name="task-content" id="task-content" class="task-input input-item">
+                <input type="date" name="task-date" id="task-date" class="task-input input-item">
             </label>
-            <input type="button" value="-" onclick="removeTask(this)" />
+            <input type="button" value="-" onclick="removeTask(this)" class="removeTask"/>
         </div>
     </div>
     `;
@@ -20,3 +21,12 @@ function removeTask(input) {
         .getElementById("tasks-container")
         .removeChild(input.parentNode.parentNode.parentNode);
 }
+
+// const headerHeight = document.querySelector("header").clientHeight;
+// const main = document.querySelector("main");
+// main.style.marginTop = headerHeight + "px";
+
+// window.addEventListener('', () => {
+//     headerHeight = document.querySelector("header").clientHeight;
+//     main.style.marginTop = headerHeight + "px";
+// });
