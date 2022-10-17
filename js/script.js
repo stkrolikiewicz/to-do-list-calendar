4edf14b3e43dc7f4d7629689e58db7d76ed70310
stachkrol <st.krolikiewicz@gmail.com>
2022-10-17 16:56:44 +0200

    Removing bootstrap library and changing display to flexbox

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ function addTask() {
     const newTask = document.createElement("form");
     newTask.className = "task";
     newTask.innerHTML = `
-    <div class="row">
-        <div class="col-12">
-            <label>
-                <input type="checkbox" name="task-chekbox" id="task-checkbox" class="task-input input-item">
-                <input type="text" name="task-content" id="task-content" class="task-input input-item">
-                <input type="date" name="task-date" id="task-date" class="task-input input-item">
-            </label>
-            <input type="button" value="-" onclick="removeTask(this)" class="removeTask"/>
-        </div>
+    <div class="task">
+        <label>
+            <input type="checkbox" name="task-chekbox" id="task-checkbox" class="task-input input-item">
+            <input type="text" name="task-content" id="task-content" class="task-input input-item">
+            <input type="date" name="task-date" id="task-date" class="task-input input-item">
+        </label>
+        <input type="button" value="-" onclick="removeTask(this)" class="removeTask"/>
     </div>
     `;
     document.getElementById("tasks-container").appendChild(newTask);
@@ -21,12 +19,3 @@ function removeTask(input) {
         .getElementById("tasks-container")
         .removeChild(input.parentNode.parentNode.parentNode);
 }
-
-// const headerHeight = document.querySelector("header").clientHeight;
-// const main = document.querySelector("main");
-// main.style.marginTop = headerHeight + "px";
-
-// window.addEventListener('', () => {
-//     headerHeight = document.querySelector("header").clientHeight;
-//     main.style.marginTop = headerHeight + "px";
-// });
